a216ffa2796c98c69b0ff0789ce2bdba1a360aaa
Timur Nabiullin <timurkre@gmail.com>
2022-12-06 22:21:30 +0600

    task commit

---
+++ task3/script.js
@@ -0,0 +1,9 @@
+const trafficLightEl = document.querySelector('#trafficLight');
+
+function makeGreen() {
+    trafficLightEl.style.background = ('green');
+    trafficLightEl.removeEventListener('click', makeGreen);
+    trafficLightEl.addEventListener('click', makeYellow);
+}
+
+trafficLightEl.addEventListener('click', makeGreen);
